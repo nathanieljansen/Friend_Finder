4ae80d1206ab8686dd8e558e32fc369c0c079f1f
Nathaniel Jansen <nathanieljansen@gmail.com>
2018-04-29 13:33:58 -0600

    pushing values of scores into array

--- public/app.js
+++ public/app.js
@@ -13,7 +13,8 @@ $(function () {
     event.preventDefault();
     let friends = {
       name: $(".yourName").val().trim(),
-      photo: $(".yourPicture").val().trim()
+      photo: $(".yourPicture").val().trim(),
+      scores: [$(".choice1").val(), $(".choice2").val(), $(".choice3").val(), $(".choice4").val(), $(".choice5").val(), $(".choice6").val(), $(".choice7").val(), $(".choice8").val(), $(".choice9").val(), $(".choice10").val(),]
     };
     // Question: What does this code do??
     $.post("/api/friends", friends).then(function () {
